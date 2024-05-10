566b2f41194ddc08f235dd953e96a7825fbd0b8e
fedtoscano <federicotoscano611@gmail.com>
2024-05-10 18:11:16 +0200

    testo il DOM su una condizione

--- script/script.js
+++ script/script.js
@@ -1,9 +1,20 @@
-//Creo un ciclo for
+//Seleziono globalmente il div container poiché mi servirà all'interno dei condizionali
 
+const containerEl = document.getElementById('container');
+
+//Creo un ciclo for
 for (i=1; i<=100; i++){
     // Definisco le condizioni
     if(i%3===0 && i%5===0){
         console.log("FizzBuzz");
+        
+        //Aggiorno il DOM creando un nuovo elemento
+        const newEl = document.createElement("div")
+        newEl.classList.add("element")
+        newEl.innerHTML = "FizzBuzz"
+
+        //Aggiungo l'elemento al div #container
+        containerEl.appendChild(newEl);
     }
     
     else if(i%5===0){
