f0ca6882bac22102953ed7487450d37ecc41cec2
fedtoscano <federicotoscano611@gmail.com>
2024-05-10 18:44:51 +0200

    aggiunta di foglio di stile e ritocchi finali allo script

--- script/script.js
+++ script/script.js
@@ -5,13 +5,18 @@ const containerEl = document.getElementById('container');
 //Creo un ciclo for
 for (i=1; i<=100; i++){
     // Definisco le condizioni
-    if(i%3===0 && i%5===0){
-        console.log("FizzBuzz");
-        
+    if(i%3===0 && i%5===0){        
         //Aggiorno il DOM creando un nuovo elemento
         const newEl = document.createElement("div")
-        newEl.classList.add("element")
-        newEl.innerHTML = "FizzBuzz"
+        newEl.classList.add("element", "fizzbuzz")
+
+        //Creo l'elemento che contiene il testo all'interno di newEl
+        const newELText = document.createElement("div")
+        newELText.classList.add("element-text")
+
+        //Aggiungo newELText a newEl
+        newELText.innerHTML = "FizzBuzz!"
+        newEl.appendChild(newELText)
 
         //Aggiungo l'elemento al div #container
         containerEl.appendChild(newEl);
@@ -19,31 +24,41 @@ for (i=1; i<=100; i++){
     
     else if(i%5===0){
         const newEl = document.createElement("div")
-        newEl.classList.add("element")
-        newEl.innerHTML = "Buzz"
+        const newELText = document.createElement("div")
+
+        newELText.classList.add("element-text")
+        newEl.classList.add("element", "buzz")
+
+        newEl.appendChild(newELText)
+        newELText.innerHTML = "Buzz!"
 
         containerEl.appendChild(newEl);
-
-        console.log("Buzz");
     } 
     
     else if (i%3===0){
         const newEl = document.createElement("div")
-        newEl.classList.add("element")
-        newEl.innerHTML = "Fizz"
-        
+        const newELText = document.createElement("div")
+
+        newELText.classList.add("element-text")
+        newEl.classList.add("element", "fizz")
+
+        newEl.appendChild(newELText)
+        newELText.innerHTML = "Fizz!"
+
         containerEl.appendChild(newEl);
-        console.log("Fizz");
     } 
     
     else{
         const newEl = document.createElement("div")
-        newEl.classList.add("element")
-        newEl.innerHTML = i
+        const newELText = document.createElement("div")
+
+        newELText.classList.add("element-text")
+        newEl.classList.add("element", "number")
+
+        newEl.appendChild(newELText)
+        newELText.innerHTML = i
         
         containerEl.appendChild(newEl);
-
-        console.log(i);
     }
 }
 
