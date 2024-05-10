1e5440bce98e09e718d95d5b3108c668b60819ac
fedtoscano <federicotoscano611@gmail.com>
2024-05-10 17:51:57 +0200

    testo la logica del condizionale

---
+++ script/script.js
@@ -0,0 +1,13 @@
+//Creo un ciclo for
+
+for (i=1; i<=100; i++){
+    // Definisco le condizioni
+    if(i%3===0){
+        console.log("Fizz")
+    } else{
+        console.log(i)
+    }
+    
+    // console.log(i)
+}
+
